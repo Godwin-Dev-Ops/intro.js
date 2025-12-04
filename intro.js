3ccf90a5c66b70e1ed600e33d9e6235850651d95
Godwin-Dev-Ops <godwinerharuyi@gmail.com>
2025-12-05 00:05:04 +0100

    This contains my primary informations

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Erharuyi Godwin";
+const age = 29;
+const favourite_Programming_Language = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Programming Language:", favourite_Programming_Language);
